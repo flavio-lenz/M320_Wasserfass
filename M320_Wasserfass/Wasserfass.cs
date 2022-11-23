a5d1cf5ffbca13539cf37e3216303b7c8e2f965d
Flavio Lenz <info@flavio-lenz.ch>
2022-11-23 11:38:34 +0100

    rewrite code

--- M320_Wasserfass/Wasserfass.cs
+++ M320_Wasserfass/Wasserfass.cs
@@ -1,6 +1,33 @@
-﻿namespace gbssg.flenz.m320.wasserfass;
+﻿using System.ComponentModel;
+
+namespace gbssg.flenz.m320.wasserfass;
 
 public class Wasserfass
 {
-    
+    private int minimalPegel;
+    private int maximalPegel;
+    public int fuellstand;
+    public int kapazitaet;
+
+    public int getFuellstand()
+    {
+        return fuellstand;
+    }
+    public int getKapazitaet()
+    {
+        return kapazitaet;
+    }
+
+    public void befuellen(int menge)
+    {
+        fuellstand = fuellstand + menge;
+    }
+    public void entnehmen(int menge)
+    {
+        fuellstand = fuellstand - menge;
+    }
+    public void entleeren()
+    {
+        fuellstand = minimalPegel;
+    }
 }
